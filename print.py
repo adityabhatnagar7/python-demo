a895a5c07b28922400e682932708221a293f28e6
Aditya Bhatnagar <158065787+adityabhatnagar7@users.noreply.github.com>
2024-02-01 19:28:52 +0530

    Create print.py

---
+++ print.py
@@ -0,0 +1,9 @@
+def myfun(input_opr):
+    print("Orginal opr:",input_opr)
+    
+    length=len (input_opr)
+    print("length of the opr:",length)
+    
+input_opr=input("Enter the opr:- ")
+    
+myfun(input_opr)
